452355770af3c514b2d4473e986c08750ea4f93a
MGibran <maikgibranr@gmail.com>
2019-04-20 22:56:13 +0700

    Add status command
    # Fix wrong class used on unit test

--- src/main/java/com/maikrantetasik/parkinglot/services/commands/StatusCommands.java
+++ src/main/java/com/maikrantetasik/parkinglot/services/commands/StatusCommands.java
@@ -1,11 +1,29 @@
 package com.maikrantetasik.parkinglot.services.commands;
 
+import com.maikrantetasik.parkinglot.entities.Car;
 import com.maikrantetasik.parkinglot.entities.CommandResult;
 import com.maikrantetasik.parkinglot.entities.ParkingLot;
+
+import java.util.Map;
 
 public class StatusCommands implements ParkingLotCommands {
 
     public CommandResult execute(ParkingLot parkingLot, String[] args) {
-        return null;
+        if (parkingLot == null) {
+            throw new IllegalStateException("Invalid parking lot state");
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Slot No. \t Registration No \t Colour");
+        builder.append(System.lineSeparator());
+
+        for (Map.Entry<Integer, Car> park : parkingLot.getFilledSlots().entrySet()) {
+            int slot = park.getKey();
+            Car car = park.getValue();
+            builder.append(String.format("%s \t\t %s \t\t %s", slot, car.getRegNumber(), car.getColor()));
+            builder.append(System.lineSeparator());
+        }
+
+        return new CommandResult(parkingLot, builder.toString());
     }
 }
--- src/test/java/com/maikrantetasik/parkinglot/services/commands/StatusCommandsTest.java
+++ src/test/java/com/maikrantetasik/parkinglot/services/commands/StatusCommandsTest.java
@@ -18,7 +18,7 @@ public class StatusCommandsTest {private ParkingLotCommands commands;
 
     @BeforeEach
     void setUp() {
-        commands = new ParkCommands();
+        commands = new StatusCommands();
 
         Map<Integer, Car> filledSlots = new HashMap<>();
         filledSlots.put(1, new Car("white", "5N1AA0N"));
