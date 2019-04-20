52c6594e20e0b61427009ee787ed042ebc837864
MGibran <maikgibranr@gmail.com>
2019-04-20 19:58:19 +0700

    Add test cases for command controller

---
+++ src/test/java/com/maikrantetasik/parkinglot/services/ParkingLotCommandControllerTest.java
@@ -0,0 +1,97 @@
+package com.maikrantetasik.parkinglot.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingLotCommandControllerTest {
+    private CommandController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ParkingLotCommandController();
+    }
+
+    @Test
+    void shouldHaveCreateCommand() {
+        String[] args = new String[] { "create_parking_lot", "6" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveParkCommand() {
+        String[] args = new String[] { "park", "KA-01-HH-1234", "White" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveLeaveCommand() {
+        String[] args = new String[] { "leave", "4" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveStatusCommand() {
+        String[] args = new String[] { "status" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveSearchRegNumbersByColorCommand() {
+        String[] args = new String[] { "registration_numbers_for_cars_with_colour", "white" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveSearchSlotNumbersByColorCommand() {
+        String[] args = new String[] { "slot_numbers_for_cars_with_colour", "white" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @Test
+    void shouldHaveSearchSlotNumbersByRegNumberCommand() {
+        String[] args = new String[] { "slot_number_for_registration_number", "MH-04-AY-1111" };
+
+        assertDoesNotThrow(() -> controller.execute(args));
+        String res = controller.execute(args);
+        assertNotNull(res);
+        assertNotEquals("", res);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { "test", "-1", "((!&&@())__$$#$#" })
+    void shouldThrowOnUnexpectedCommands(String command) {
+        String[] args = new String[] { command };
+
+        assertThrows(IllegalArgumentException.class, () -> controller.execute(args));
+    }
+}
