6883a6598ca8e11071c1d5c73beec3b6eaa774d1
MGibran <maikgibranr@gmail.com>
2019-04-22 23:19:19 +0700

    Fix status output spacing

--- src/main/java/com/maikrantetasik/parkinglot/services/commands/StatusCommands.java
+++ src/main/java/com/maikrantetasik/parkinglot/services/commands/StatusCommands.java
@@ -14,14 +14,13 @@ public class StatusCommands implements ParkingLotCommands {
         }
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Slot No. \t Registration No \t Colour");
-        builder.append(System.lineSeparator());
+        builder.append("Slot No.    Registration No    Colour");
 
         for (Map.Entry<Integer, Car> park : parkingLot.getFilledSlots().entrySet()) {
             int slot = park.getKey();
             Car car = park.getValue();
-            builder.append(String.format("%s \t\t %s \t\t %s", slot, car.getRegNumber(), car.getColor()));
             builder.append(System.lineSeparator());
+            builder.append(String.format("%s           %s      %s", slot, car.getRegNumber(), car.getColor()));
         }
 
         return new CommandResult(parkingLot, builder.toString());
